5ea798731b54a09e17f297fc136434d159b7b839
varshini <varshini27101999@gmail.com>
2021-09-03 18:17:52 +0530

    todo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
+  const variable=props.subject
+  console.log(props)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, {variable}!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
